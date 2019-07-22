dcaac1750211d2297dd1529809a1b8f4de76c9ff
Dominykas Sipelis <dsipel2@uic.edu>
2019-07-22 11:56:36 -0500

    Added Status change and fixed some syntax

--- bot.py
+++ bot.py
@@ -1,26 +1,31 @@
 import discord
 import random
 
+from discord import Game
 from discord.ext.commands import Bot
 
 TOKEN = 'NA'
 
 bot = Bot(command_prefix='?')
 
+
 @bot.event
 async def on_ready():
+    game = discord.Game("Listening to these Idiots")
+    await bot.change_presence(activity=game)
     print('logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('--------')
 
-@bot.command(name = 'roll',
-             description = 'Rolls random number up to specified limit',
-             brief = '[upper limit]',
-             aliases = ['dice'])
+
+@bot.command(name='roll',
+             description='Rolls random number up to specified limit',
+             brief='[upper limit]',
+             aliases=['dice'])
 async def roll(ctx, limit: int):
-    rNum = random.randint(1,limit)
-    await ctx.send(str(rNum))
+    rnum = random.randint(1, limit)
+    await ctx.send(str(rnum))
 
 
 bot.run(TOKEN)
