be79ed7205e2acc085c95805298f654b566eba1d
Dominykas Sipelis <dsipel2@uic.edu>
2019-07-22 13:18:14 -0500

    added PickIdiot command and error handling

--- bot.py
+++ bot.py
@@ -1,12 +1,48 @@
 import discord
 import random
+import time
+import sys
+import traceback
 
 from discord import Game
-from discord.ext.commands import Bot
+from discord.ext import commands
 
-TOKEN = 'NA'
+TOKEN = ''
 
-bot = Bot(command_prefix='?')
+bot = commands.Bot(command_prefix='?')
+
+
+def get_time():
+    localtime = time.asctime(time.localtime(time.time()))
+    return localtime
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if hasattr(ctx.command, 'on_error'):
+        return
+
+    ignored = (commands.CommandNotFound)
+
+    error = getattr(error, 'original', error)
+
+    if isinstance(error, ignored):
+        return
+
+    elif isinstance(error, commands.DisabledCommand):
+        return await ctx.send(str(ctx.command) + 'has been disabled you moron')
+
+    elif isinstance(error, commands.NoPrivateMessage):
+        try:
+            return await ctx.author.send(str(ctx.command) + 'cannot be used in Private Messages you idiot')
+        except:
+            pass
+
+    elif isinstance(error, commands.UserInputError):
+        return await ctx.send('Thats the wrong syntax you fucking idiot')
+
+    print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+    traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 
 
 @bot.event
@@ -21,11 +57,34 @@ async def on_ready():
 
 @bot.command(name='roll',
              description='Rolls random number up to specified limit',
-             brief='[upper limit]',
+             brief='roll from 1 to limit',
              aliases=['dice'])
 async def roll(ctx, limit: int):
     rnum = random.randint(1, limit)
     await ctx.send(str(rnum))
+
+
+@roll.error
+async def roll_handler(ctx, error):
+    if isinstance(error, commands.UserInputError):
+        await ctx.send('It\'s supposed to be a number after ?roll you moron')
+
+
+@bot.command(name='PickIdiot',
+             description='picks a random idiot from the idiot tag',
+             brief='picks a random idiot',
+             aliases=['pickidiot', 'Pickidiot', 'pickIdiot'],
+             pass_context=True)
+async def pick_idiot(ctx):
+    server = bot.get_guild(156092180090322944)
+    role = discord.utils.get(server.roles, name='idiot')
+    idiots = []
+    for member in server.members:
+        if role in member.roles:
+            idiots.append(member)
+    random_num = random.randint(0, len(idiots)-1)
+    random_idiot = idiots.pop(random_num)
+    await ctx.send(random_idiot.display_name)
 
 
 bot.run(TOKEN)
